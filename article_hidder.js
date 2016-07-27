95f9378eff24d84d45b401675bc976cf9efa4a85
Bassim <bassim.matar@gmail.com>
2016-07-27 16:50:15 +0200

    Update article_hidder.js

--- article_hidder.js
+++ article_hidder.js
@@ -7,9 +7,6 @@ Author: Bassim Matar
 
 Article_hidder =
 {
-	btn_msg_close: 'Fermer',
-	btn_msg_open: 'En savoir plus...',
-
 	config_each_article()
 	{
 		var that = this;
@@ -32,27 +29,45 @@ Article_hidder =
 
 	change_state(is_hidden, btn)
 	{
+		this.remove_if_no_hide_back(btn);
+
 		$(btn)
 			.data('is_hidden', !is_hidden)
 			.html(is_hidden ? this.btn_msg_close : this.btn_msg_open)
 			.parent().find('.article_hidder')
-			.css('display', is_hidden ? 'block' : 'none')
+			.css('display', is_hidden ? 'block' : 'none');
+	},
+
+	remove_if_no_hide_back(btn)
+	{
+		if (!this.hide_back)
+		{
+			$(btn).hide();
+		}
 	},
 
 	bind_event()
 	{
 		var that = this;
-		$('.article_hidder_btn').on('click', function()
+		$('.article_hidder_btn').unbind('click').on('click', function()
 		{
 			that.change_state($(this).data('is_hidden'), this);
 		});
 	},
 
-	init()
+	init(hide_back, btn_msg_close, btn_msg_open)
 	{
+		this.hide_back = hide_back;
+		this.btn_msg_close = btn_msg_close;
+		this.btn_msg_open = btn_msg_open;
+
 		this.config_each_article();
 		this.bind_event();
 	}
 }
 
-Article_hidder.init();
+Article_hidder.init(
+	hide_back = true,
+	btn_msg_close = 'Fermer',
+	btn_msg_open = 'En savoir plus...'
+);
